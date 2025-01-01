18d44091507c655ed1f9247ab122d1015ec0aed2
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2025-01-01 17:25:50 +0600

    move index.js file

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,16 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 // src/index.ts
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
-const db_1 = require("../config/db");
-const feedbackRoutes_1 = __importDefault(require("../routes/feedbackRoutes"));
+const db_1 = require("./config/db");
+const feedbackRoutes_1 = __importDefault(require("./routes/feedbackRoutes"));
 dotenv_1.default.config();
 // Create a new express app instance
 const app = (0, express_1.default)();
@@ -18,8 +20,8 @@ app.use(express_1.default.json());
 (0, db_1.connectDB)();
 app.use("/api/feedback", feedbackRoutes_1.default);
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
